cea335b47603c5a0cf73498b884f02483c1d4853
RachitaBas <rachitarachita123@gmail.com>
2021-11-16 17:54:25 +0545

    the fetch api

--- sandbox.js
+++ sandbox.js
@@ -1,37 +1,10 @@
-const getTodos = (resource) => { 
-    return new Promise((resolve,reject)=>{
-const request= new XMLHttpRequest();
-request.addEventListener('readystatechange',()=>{
-if(request.readyState === 4 && request.status === 200 ){
-  
-    const data = JSON.parse(request.responseText);
-   resolve(data);
+ //fetch api
+ fetch('todos/rachita.json').then((response) => { //data that we want to fetch
+console.log('resolved',response);
+ return response.json();//gets us response data so taht we can use that data i.e it returns a promise
+}).then(data => {//retruning promise avove gives us a resolved  promise that gives us data that we went out to fetch
+    console.log(data);
+}).catch((err)=>{
+    console.log('rejected',err);
+});
     
-}
-else if(request.readyState === 4){
-reject('error getting resource');
-}
-});
- 
-request.open('GET',resource);
-request.send();
-});
-};
-getTodos('todos/rachita.json').then(data => {
-    console.log('promise 1 resolved:',data);
-    return getTodos('todos/basnet.json');
-}).then(data => {
-    console.log('promise 2 resolved:',data);
-    
-
-
-}).catch((err) => {
-    console.log('promise rejected:',err);
-});
-
-// const getSomething=()=>{
-//     return new Promise((resolve,reject)=>{ //parameters are built into the promise api in js and can automticlly receive this
-// resolve('some data');
-// reject('some error');
-//     };
-
