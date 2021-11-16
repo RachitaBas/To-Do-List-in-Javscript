5142597a975f34b0827cdc1f15647a1ce8848ca6
RachitaBas <rachitarachita123@gmail.com>
2021-11-16 16:24:02 +0545

    promises in js

--- sandbox.js
+++ sandbox.js
@@ -1,27 +1,33 @@
-const getTodos = (resource,callback) => { 
+const getTodos = (resource) => { 
+    return new Promise((resolve,reject)=>{
 const request= new XMLHttpRequest();
 request.addEventListener('readystatechange',()=>{
 if(request.readyState === 4 && request.status === 200 ){
   
     const data = JSON.parse(request.responseText);
-    callback(undefined,data);
+   resolve(data);
     
 }
 else if(request.readyState === 4){
-    callback('could not fetch the data',undefined);
+reject('error getting resource');
 }
 });
  
 request.open('GET',resource);
 request.send();
-};
-console.log(1);
-console.log(2);
-
-getTodos('todos/rachita.json',(err, data) => {  //callback within call abck that is callback nesting called callback hell
-    console.log(data);
-    getTodos('todos/basnet.json',(err, data) => {  
-        console.log(data);
-    });
 });
+};
+getTodos('todos/rachita.json').then(data => {//if location url of json  is wrong it is rejected 
+    //returning promise with the function that fires with data we get back when there is resolve case
+    console.log('promise resolved:',data);
+
+}).catch((err) => {
+    console.log('promise rejected:',err);
+});
+
+// const getSomething=()=>{
+//     return new Promise((resolve,reject)=>{ //parameters are built into the promise api in js and can automticlly receive this
+// resolve('some data');
+// reject('some error');
+//     };
 
