1b03092c13eefd8ed0a24f47c21c91c9710781f7
RachitaBas <rachitarachita123@gmail.com>
2021-11-16 14:39:01 +0545

    callback functions

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,31 @@
+const getTodos = (callback) => { //define callback here and call the gettodos function below
 const request= new XMLHttpRequest();//SEND REQUEST TO GET DATA
 request.addEventListener('readystatechange',()=>{//ADDEVENTLISTENER TO TRACK PROGRESS OF REQUEST AND readystate to tell request goes through different phases and 4 in total
 // console.log(request,request.readyState);//state taht current request is in 
 if(request.readyState === 4 && request.status === 200 ){//readystate is 4 so if it is in 4 then it send response text or data we can use .
-    console.log(request,request.responseText);//requesting response text if state is 4
+    callback(undefined,request.responseText);//undefined for error and second for data
+    
 }
-else if(request.readyState === 4){//thought state is 4 but some problm then fire following console
-    console.log('could not fetch the data');
+else if(request.readyState === 4){//thought state is 4 but some problem then fire following console
+    callback('could not fetch the data',undefined);//if not readystate found cannot fetch data
 }
 });
  
 request.open('GET',' https://jsonplaceholder.typicode.com/todos/');//get to get some data and this is just setting up request and second arg is endpoint we want to get data from
 request.send();//send request
+};
+console.log(1);
+console.log(2);
+
+getTodos((err, data) => {  ////call gettodos function here and paasing callback function as argument
+    console.log('callback fired');
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(data);
+    }
+});
+console.log(3);
+console.log(4);
+//aynchnous code because 12 then 34 is called first then callback is fired shown as it starts again after receiving data till then 3 and 4 is executed and then data is shown below it
